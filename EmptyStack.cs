09ec75d0e8e05b5894e6cb3a90406fa70af0828e
jayukasode23 <jayukasode2398@gmail.com>
2022-10-17 20:40:35 +0530

    Added Empty Stack

---
+++ EmptyStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class EmptyStack
+    {
+        LinkedList<int> list;
+
+        public void CheckStack()
+        {
+            list = new LinkedList<int>();
+        }
+        public void Add(int n)
+        {
+            list.AddLast(n);
+        }
+        public void PeakPop()
+        {
+            foreach (int num in list.ToList())
+            {
+                Console.WriteLine("Remove Element " + num + "From Linked List");
+                list.Remove(num);
+            }
+        }
+    }
+}
