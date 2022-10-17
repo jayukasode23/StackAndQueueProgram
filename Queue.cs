af58434dae4b6ec7e5a8ebccc0e65e8edf6544fa
jayukasode23 <jayukasode2398@gmail.com>
2022-10-17 20:42:23 +0530

    Added Queue File

---
+++ Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class Queue
+    {
+        public Node front, rear;
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+        //Method To add an Key To The Queue.
+        public void Enqueue(int key)
+        {
+            //Create a new LL node.
+            Node temp = new Node(key);
+
+            //If queue is empty , then new node is front and rear both.
+            if (this.rear == null)
+            {
+
+                this.front = this.rear = temp;
+                return;
+
+            }
+            //Add new node at the end of quque andchange rear.
+            this.rear.Next = temp;
+            this.rear = temp;
+            Console.WriteLine("The Item inserted into Queue is {0}", key);
+
+        }
+
+
+    }
+}
+  
