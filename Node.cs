a9294b06f21f992470f9ed41123fa8e145a85c30
jayukasode23 <jayukasode2398@gmail.com>
2022-10-17 20:39:08 +0530

    Added Node File

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class Node
+    {
+        public int Key;
+        public Node Next;
+
+        public Node(int Key)
+        {
+            this.Key = Key;
+        }
+    }
+}
+
